858dc20a51403d1e9ac26c3a66dfd8670ce42a7b
sohelranalive <sohel.cse37@gmail.com>
2024-11-21 00:09:05 +0100

    updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,67 +1,111 @@
-import { FaGithub, FaLinkedin, FaDownload } from 'react-icons/fa';
-import htmlImg from '../assets/html.png'
-import cssImg from '../assets/css.png'
-import tailwindImg from '../assets/tailwind.png'
-import jsImg from '../assets/icon-javascript-tran.png'
-import reactImg from '../assets/icon-react-js-tran.png'
-import nodeImg from '../assets/icon-node-js-tran.png'
-import { TypeAnimation } from 'react-type-animation';
+import { FaDownload, FaGithub, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import bannerIMG from "../assets/bannerIMG.png";
+import cssImg from "../assets/css.png";
+import htmlImg from "../assets/html.png";
+import jsImg from "../assets/icon-javascript-tran.png";
+import nodeImg from "../assets/icon-node-js-tran.png";
+import reactImg from "../assets/icon-react-js-tran.png";
+import tailwindImg from "../assets/tailwind.png";
 
 const Banner = () => {
-    return (
-        <div className="bg-slate-50 px-10 md:px-20 py-20 md:h-[calc(100vh-68px)]">
-            {/* tittle and image */}
-            <div className='flex flex-col-reverse md:flex md:flex-row gap-8 md:gap-0'>
-                {/* text area */}
-                <div className='md:w-1/2 px-0 md:px-20 space-y-6 flex flex-col justify-center'>
-                    <h1 className="text-4xl font-bold">Full Stack Developer</h1>
-                    <h3 className="text-2xl">
-                        <TypeAnimation
-                            sequence={['Hello, I am Sohel Rana, a passionate web developer with expertise in the MERN stack. Currently, I am pursuing my Masters in Engineering in Information Technology while residing in Frankfurt, Germany.']}
-                            wrapper="span"
-                            speed={50}
-                        />
-                    </h3>
-                    <div className='flex items-center space-x-4 text-3xl'>
-                        <a href='https://github.com/sohelranalive'><FaGithub /></a>
-                        <a href='https://www.linkedin.com/in/sohelranalive/'><FaLinkedin /></a>
-                        <a href="https://drive.google.com/uc?export=download&id=1Y9DFvQf2pMqnzIdtnyZPfI8LXjgZn-0U" download><FaDownload /></a>
-                    </div>
-                </div>
-
-                {/* Image area */}
-                <div className='md:w-1/2 px-0 md:px-20 h-[400px]'>
-                    <div className='banner-img h-full w-full'></div>
-                </div>
+  return (
+    <div className="bg-slate-50 flex flex-col px-10 sm:px-10 md:px-20">
+      {/* Title and image banner */}
+      <div className="min-h-[300px] md:min-h-[400px] flex flex-col-reverse md:flex md:flex-row">
+        {/* Text area */}
+        <div className="min-h-[300px] md:min-h-[400px] w-full md:w-1/2 px-0 md:px-10 lg:px-20 space-y-1 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold">
+            <TypeAnimation
+              sequence={["MD SOHEL RANA"]}
+              wrapper="span"
+              speed={30}
+            />
+          </h1>
+          <h1 className="text-2xl font-bold italic">Full Stack Developer</h1>
+          <br />
+          <h1 className="text-xl text-justify">
+            I’m a passionate Software developer with expertise in the MERN stack
+            and a strong focus on creating exceptional user experiences.
+            Currently pursuing a Master’s in Engineering in Information
+            Technology at Frankfurt University of Applied Sciences, I’m based in
+            Frankfurt, Germany.
+          </h1>
+          <br />
+          <div className="md:flex items-center text-xl sm:text-2xl">
+            <div className="flex items-center space-x-4 text-2xl sm:text-3xl">
+              <a href="https://github.com/sohelranalive">
+                <FaGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/sohelranalive/">
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://drive.google.com/uc?export=download&id=1Y9DFvQf2pMqnzIdtnyZPfI8LXjgZn-0U"
+                download
+              >
+                <FaDownload />
+              </a>
             </div>
-
-            {/* tech stack */}
-            <div className='md:flex items-center text-2xl px-0 md:px-20 space-y-2 md:space-y-0 mt-10 md:mt-0'>
-                <div className='md:pr-2 md:border-r-2 border-blue-400'>Tech Stack</div>
-
-                <div className='md:ps-2 flex gap-4 flex-wrap'>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={htmlImg} alt="" className='h-full w-full' />
-                    </div>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={cssImg} alt="" className='h-full w-full' />
-                    </div>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={tailwindImg} alt="" className='h-full w-full' />
-                    </div>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={jsImg} alt="" className='h-full w-full' />
-                    </div>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={reactImg} alt="" className='h-full w-full' />
-                    </div>
-                    <div className='h-12 w-12 p-2 custom-shadow rounded-full bg-white'>
-                        <img src={nodeImg} alt="" className='h-full w-full' />
-                    </div>
-                </div>
-            </div>
+          </div>
+          {/* <h1 className="text-3xl sm:text-4xl font-bold">MD SOHEL RANA</h1>
+          <h1 className="text-3xl sm:text-3xl font-bold">
+            Full Stack Developer
+          </h1>
+          <h3 className="text-lg sm:text-2xl text-justify">
+            <TypeAnimation
+              sequence={[
+                "Hello, I am Sohel Rana, a passionate web developer with expertise in the MERN stack. Currently, I am pursuing my Masters in Engineering in Information Technology while residing in Frankfurt, Germany.",
+              ]}
+              wrapper="span"
+              speed={50}
+            />
+          </h3> */}
         </div>
-    );
+        {/* Image area */}
+        <div className="min-h-[300px] md:min-h-[400px] w-full md:w-1/2 px-0 md:px-10 lg:px-20">
+          {/* need to put image here */}
+          <img
+            src={bannerIMG}
+            alt="Your Image"
+            className="h-[400px] md:h-[700px] w-full object-cover"
+          />
+        </div>
+      </div>
+
+      {/* Contact and Resume download */}
+
+      {/* Tech stack */}
+      <div className="md:flex items-center text-xl sm:text-2xl px-0 md:px-10 lg:px-20 py-2 mt-2">
+        <div className="md:pr-4 md:border-r-4 border-blue-400">Tech Stack</div>
+
+        <div className="md:pl-4 flex gap-4 flex-wrap">
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img src={htmlImg} alt="HTML" className="h-full w-full" />
+          </div>
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img src={cssImg} alt="CSS" className="h-full w-full" />
+          </div>
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img
+              src={tailwindImg}
+              alt="Tailwind CSS"
+              className="h-full w-full"
+            />
+          </div>
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img src={jsImg} alt="JavaScript" className="h-full w-full" />
+          </div>
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img src={reactImg} alt="React" className="h-full w-full" />
+          </div>
+          <div className="h-12 w-12 p-2 custom-shadow rounded-full bg-white">
+            <img src={nodeImg} alt="Node.js" className="h-full w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/ProjectOne.jsx
+++ src/components/ProjectOne.jsx
@@ -1,124 +1,223 @@
-import { FaGithub } from 'react-icons/fa';
-import { BsBoxArrowUpRight } from 'react-icons/bs';
-import easyMovesHome from '../assets/Home.jpg'
-import easyMovesPopularClasses from '../assets/Popularclass.jpg'
-import easyMovesPopularInstructors from '../assets/PopularInstructor.jpg'
-import easyMovesFooter from '../assets/Footer.jpg'
-import easyMovesClasses from '../assets/classes.jpg'
-import easyMovesInstructor from '../assets/instructor.jpg'
-import easyMovesLogin from '../assets/login.jpg'
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import easyMovesClasses from "../assets/EasyMoves/classes.jpg";
+import easyMovesFooter from "../assets/EasyMoves/Footer.jpg";
+import easyMovesHome from "../assets/EasyMoves/Home.jpg";
+import easyMovesInstructor from "../assets/EasyMoves/instructor.jpg";
+import easyMovesLogin from "../assets/EasyMoves/login.jpg";
+import easyMovesPopularClasses from "../assets/EasyMoves/PopularClass.jpg";
+import easyMovesPopularInstructors from "../assets/EasyMoves/PopularInstructor.jpg";
 
 const ProjectOne = () => {
+  useEffect(() => {
+    const removeHashFromURL = () => {
+      if (window.location.hash) {
+        window.history.replaceState(null, null, window.location.pathname);
+      }
+    };
+    removeHashFromURL();
+    window.addEventListener("hashchange", removeHashFromURL);
+    return () => {
+      window.removeEventListener("hashchange", removeHashFromURL);
+    };
+  }, []);
 
-    useEffect(() => {
-        const removeHashFromURL = () => {
-            if (window.location.hash) {
-                window.history.replaceState(null, null, window.location.pathname);
-            }
-        };
-        removeHashFromURL();
-        window.addEventListener('hashchange', removeHashFromURL);
-        return () => {
-            window.removeEventListener('hashchange', removeHashFromURL);
-        };
-    }, []);
-
-    return (
-        <div className="custom-shadow rounded-2xl p-8 md:flex space-y-4 md:space-y-0">
-            {/* carousel */}
-            <div className="md:w-7/12 h-full">
-                <div className="carousel w-full">
-                    {/* slide 1 */}
-                    <div id="emslide1" className="carousel-item relative w-full">
-                        <img src={easyMovesHome} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide7" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide2" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 2 */}
-                    <div id="emslide2" className="carousel-item relative w-full">
-                        <img src={easyMovesPopularClasses} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide1" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide3" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 3 */}
-                    <div id="emslide3" className="carousel-item relative w-full">
-                        <img src={easyMovesPopularInstructors} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide2" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide4" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 4 */}
-                    <div id="emslide4" className="carousel-item relative w-full">
-                        <img src={easyMovesFooter} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide3" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide5" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 5 */}
-                    <div id="emslide5" className="carousel-item relative w-full">
-                        <img src={easyMovesClasses} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide4" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide6" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 6 */}
-                    <div id="emslide6" className="carousel-item relative w-full">
-                        <img src={easyMovesInstructor} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide5" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide7" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                    {/* slide 7 */}
-                    <div id="emslide7" className="carousel-item relative w-full">
-                        <img src={easyMovesLogin} className="w-full rounded-2xl" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#emslide6" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5">❮</a>
-                            <a href="#emslide1" className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400">❯</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="custom-shadow rounded-2xl p-8 md:flex space-y-4 md:space-y-0">
+      {/* carousel */}
+      <div className="md:w-7/12 h-full">
+        <div className="carousel w-full">
+          {/* slide 1 */}
+          <div id="emslide1" className="carousel-item relative w-full">
+            <img src={easyMovesHome} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide7"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide2"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
             </div>
-
-            {/* Descriptions */}
-            <div className="md:w-5/12 h-full md:ps-10">
-                <h1 className='text-3xl font-bold'>EasyMoves</h1>
-                <br />
-                <p className="text-justify">
-                    An interactive Full stack MERN Application for dance class. This website basically targeted the students. In this application a normal user will be able to see different kind of classes and instructors details with some amazing functionality.
-                </p>
-                <br />
-                <div className='grid grid-cols-2 md:grid-cols-4 gap-4 justify-around'>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">ReactJS</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">Express</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">Node</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">MongoDB</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">HTML</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">Tailwind</button>
-                    <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">DaisyUI</button>
-                </div>
-                <br />
-                <div className='flex space-x-6 justify-center w-full'>
-                    <a href='https://github.com/sohelranalive/baby-soldier-client' target="_blank" rel="noopener noreferrer" className='flex space-x-2 text-2xl items-center hover:text-blue-400'>
-                        <span>Code</span>
-                        <FaGithub className='h-6 w-6' />
-                    </a>
-                    <a href='https://easy-moves-bb7bc.web.app/' target="_blank" rel="noopener noreferrer" className='flex space-x-2 text-2xl items-center hover:text-blue-400'>
-                        <span>Live Demo</span>
-                        <BsBoxArrowUpRight className='h-6 w-6' />
-                    </a>
-                </div>
+          </div>
+          {/* slide 2 */}
+          <div id="emslide2" className="carousel-item relative w-full">
+            <img src={easyMovesPopularClasses} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide1"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide3"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
             </div>
+          </div>
+          {/* slide 3 */}
+          <div id="emslide3" className="carousel-item relative w-full">
+            <img
+              src={easyMovesPopularInstructors}
+              className="w-full rounded-2xl"
+            />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide2"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide4"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+          {/* slide 4 */}
+          <div id="emslide4" className="carousel-item relative w-full">
+            <img src={easyMovesFooter} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide3"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide5"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+          {/* slide 5 */}
+          <div id="emslide5" className="carousel-item relative w-full">
+            <img src={easyMovesClasses} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide4"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide6"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+          {/* slide 6 */}
+          <div id="emslide6" className="carousel-item relative w-full">
+            <img src={easyMovesInstructor} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide5"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide7"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+          {/* slide 7 */}
+          <div id="emslide7" className="carousel-item relative w-full">
+            <img src={easyMovesLogin} className="w-full rounded-2xl" />
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+              <a
+                href="#emslide6"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400 mr-5"
+              >
+                ❮
+              </a>
+              <a
+                href="#emslide1"
+                className="btn btn-circle bg-blue-400 border-none opacity-70 hover:opacity-100 hover:bg-blue-400"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* Descriptions */}
+      <div className="md:w-5/12 h-full md:ps-10">
+        <h1 className="text-3xl font-bold">EasyMoves</h1>
+        <br />
+        <p className="text-justify">
+          An interactive Full stack MERN Application for dance class. This
+          website basically targeted the students. In this application a normal
+          user will be able to see different kind of classes and instructors
+          details with some amazing functionality.
+        </p>
+        <br />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-around">
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            ReactJS
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            Express
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            Node
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            MongoDB
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            HTML
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            Tailwind
+          </button>
+          <button className="px-2 py-3 rounded-lg shadow-xl bg-slate-100 font-bold text-xl">
+            DaisyUI
+          </button>
+        </div>
+        <br />
+        <div className="flex space-x-6 justify-center w-full">
+          <a
+            href="https://github.com/sohelranalive/baby-soldier-client"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex space-x-2 text-2xl items-center hover:text-blue-400"
+          >
+            <span>Code</span>
+            <FaGithub className="h-6 w-6" />
+          </a>
+          <a
+            href="https://easy-moves-bb7bc.web.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex space-x-2 text-2xl items-center hover:text-blue-400"
+          >
+            <span>Live Demo</span>
+            <BsBoxArrowUpRight className="h-6 w-6" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectOne;
